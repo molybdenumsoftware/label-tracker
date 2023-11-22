8f8a708c4f89a381f34647f445bc943f36d14238
Roland Fredenhagen <dev@modprog.de>
2023-11-22 09:36:07 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:api.nix

--- api.nix
+++ api.nix
@@ -1,10 +1,12 @@
 {
   rustPlatform,
   pkgs,
+  postgresql,
 }:
 rustPlatform.buildRustPackage {
   name = "api";
   cargoLock.lockFile = ./Cargo.lock;
   src = ./.;
   buildAndTestSubdir = "api";
+  checkInputs = [postgresql];
 }
