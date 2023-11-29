0ca3d9e98b35c87e3357a8f2bb729eb1036c3a6a
Jeremy Fleischman <jeremyfleischman@gmail.com>
2023-11-28 21:10:56 -0800

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:store/src/lib.rs

--- store/src/lib.rs
+++ store/src/lib.rs
@@ -9,7 +9,16 @@ pub struct Landing {
 impl Landing {
     pub const TABLE: &str = "landings";
 
-    pub fn insert(self, db: impl Connection) -> Result<()> {
+    pub async fn insert(self, connection: impl Connection) -> Result<()> {
+        // TODO: this isn't gonna compile until we have a running database for sqlx to talk to at
+        // build time.
+        sqlx::query!(
+            "INSERT INTO landings(github_pr, channel) VALUES (?, ?)",
+            self.github_pr,
+            self.channel
+        )
+        .execute(connection)
+        .await;
         todo!()
         //sqlx::query!("insert")
     }
