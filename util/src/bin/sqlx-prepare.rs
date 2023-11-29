653ef003d89d7c04a7e61e34db38710350a3bc1a
Jeremy Fleischman <jeremyfleischman@gmail.com>
2023-11-28 23:24:31 -0800

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:util/src/bin/sqlx-prepare.rs

--- util/src/bin/sqlx-prepare.rs
+++ util/src/bin/sqlx-prepare.rs
@@ -1,3 +1,15 @@
+use std::process::Command;
+use util::DatabaseContext;
+
 fn main() {
+    let database_ctx = DatabaseContext::init();
+
+    let status = Command::new("cargo")
+        .args(["sqlx", "prepare", "--workspace", "--database-url"])
+        .arg(database_ctx.db_url())
+        .status()
+        .unwrap();
+
+    assert!(status.is_successful());
     println!("hello, world")
 }
