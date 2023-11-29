50df5462624592993a88749d8c1c953b02f0bf43
Jeremy Fleischman <jeremyfleischman@gmail.com>
2023-11-28 23:01:52 -0800

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:util.nix

--- util.nix
+++ util.nix
@@ -1,15 +1,44 @@
 {
   rustPlatform,
   sqlx-cli,
+  lib,
 }:
+
+let
+  inherit (lib)
+    fileset;
+
+  sourceFiles = fileset.unions [ #<<<
+    ./util
+    ./Cargo.lock
+    ./Cargo.toml
+  ];
+in
+
+fileset.trace sourceFiles
+
 rustPlatform.buildRustPackage
 {
   pname = "sqlx-prepare";
   version = "0.1.0";
   runtimeInputs = [sqlx-cli];
-  src = ./.;
+  #<<< src = ./.;
+  src = fileset.toSource {
+    root = ./.;
+    fileset = fileset.unions [
+      ./util
+      ./Cargo.lock
+      ./Cargo.toml
+    ];
+  };
   cargoLock.lockFile = ./Cargo.lock;
   buildAndTestSubdir = "util";
+
+  preBuild = ''
+    echo "HIYAAAAAAAAAAAA";
+    ls
+    cat Cargo.toml
+  '';
 
   #<<< text = ''
   #<<<   cargo run --package util --bin
