f908a5dfdb0e7bdba4de51fe11e85253f30efa46
Jeremy Fleischman <jeremyfleischman@gmail.com>
2023-12-01 01:37:00 -0800

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:util/src/bin/db-repl.rs

--- util/src/bin/db-repl.rs
+++ util/src/bin/db-repl.rs
@@ -4,6 +4,7 @@ use util::DatabaseContext;
 
 #[tokio::main]
 async fn main() {
+    // TODO: suppress stdout/stderr from postgres? it's quite annoying when in the psql repl
     let code = DatabaseContext::with(|database_ctx| {
         let db_url = database_ctx.db_url();
         let status = Command::new("psql").arg(db_url).status().unwrap();
