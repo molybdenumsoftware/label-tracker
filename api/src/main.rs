f65f8bbb3efbb8128969ab76d7a7ab14a1b1ffab
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-12-04 13:29:35 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:api/src/main.rs

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,81 +1,11 @@
 #![warn(clippy::pedantic)]
-
 // required because rocket::launch, remove if clippy permits.
 #![allow(clippy::no_effect_underscore_binding)]
 
-use rocket::launch;
 use api::app;
-
+use rocket::launch;
 
 #[launch]
 fn rocket() -> _ {
     rocket::build().attach(app())
-}
-
-#[cfg(test)]
-mod test {
-    use rocket::{
-        figment::Figment, futures::FutureExt, http::Status, local::asynchronous::Client, Rocket,
-    };
-    use store::Landing;
-    use util::DatabaseContext;
-
-    use api::{Channel, LandedIn};
-
-    trait Rocketable {
-        fn rocket(&self) -> Rocket<rocket::Build>;
-    }
-
-    impl Rocketable for DatabaseContext {
-        fn rocket(&self) -> Rocket<rocket::Build> {
-            rocket::custom(
-                Figment::from(rocket::Config::default())
-                    .merge(("databases.data.url", self.db_url()))
-                    .merge(("log_level", rocket::config::LogLevel::Debug)),
-            )
-            .attach(super::app())
-        }
-    }
-
-    #[tokio::test]
-    async fn pr_not_found() {
-        DatabaseContext::with(|ctx: &DatabaseContext| {
-            async {
-                let client = Client::tracked(ctx.rocket()).await.unwrap();
-                let response = client.get("/landed/github/2134").dispatch().await;
-                assert_eq!(response.status(), Status::NotFound);
-                assert_eq!(response.into_string().await, Some("Pull request not found.".into()));
-            }
-            .boxed()
-        })
-        .await;
-    }
-
-    #[tokio::test]
-    async fn pr_landed_in_master() {
-        DatabaseContext::with(|ctx| {
-            async {
-                let mut connection = ctx.connection().await.unwrap();
-
-                let landing = Landing {
-                    github_pr_number: 2134.try_into().unwrap(),
-                    channel: store::Channel::new("nixos-unstable"),
-                };
-
-                landing.insert(&mut connection).await.unwrap();
-
-                let client = Client::tracked(ctx.rocket()).await.unwrap();
-                let response = client.get("/landed/github/2134").dispatch().await;
-                assert_eq!(response.status(), Status::Ok);
-                assert_eq!(
-                    response.into_json::<LandedIn>().await.unwrap(),
-                    LandedIn {
-                        channels: vec![Channel("nixos-unstable".to_owned())]
-                    }
-                );
-            }
-            .boxed()
-        })
-        .await;
-    }
 }
---
+++ api/tests/test.rs
@@ -0,0 +1,65 @@
+use rocket::futures::FutureExt;
+
+trait Rocketable {
+    fn rocket(&self) -> rocket::Rocket<rocket::Build>;
+}
+
+impl Rocketable for util::DatabaseContext {
+    fn rocket(&self) -> rocket::Rocket<rocket::Build> {
+        rocket::custom(
+            rocket::figment::Figment::from(rocket::Config::default())
+                .merge(("databases.data.url", self.db_url()))
+                .merge(("log_level", rocket::config::LogLevel::Debug)),
+        )
+        .attach(api::app())
+    }
+}
+
+#[tokio::test]
+async fn pr_not_found() {
+    util::DatabaseContext::with(|ctx: &util::DatabaseContext| {
+        async {
+            let client = rocket::local::asynchronous::Client::tracked(ctx.rocket())
+                .await
+                .unwrap();
+            let response = client.get("/landed/github/2134").dispatch().await;
+            assert_eq!(response.status(), rocket::http::Status::NotFound);
+            assert_eq!(
+                response.into_string().await,
+                Some("Pull request not found.".into())
+            );
+        }
+        .boxed()
+    })
+    .await;
+}
+
+#[tokio::test]
+async fn pr_landed_in_master() {
+    util::DatabaseContext::with(|ctx| {
+        async {
+            let mut connection = ctx.connection().await.unwrap();
+
+            let landing = store::Landing {
+                github_pr_number: 2134.try_into().unwrap(),
+                channel: store::Channel::new("nixos-unstable"),
+            };
+
+            landing.insert(&mut connection).await.unwrap();
+
+            let client = rocket::local::asynchronous::Client::tracked(ctx.rocket())
+                .await
+                .unwrap();
+            let response = client.get("/landed/github/2134").dispatch().await;
+            assert_eq!(response.status(), rocket::http::Status::Ok);
+            assert_eq!(
+                response.into_json::<api::LandedIn>().await.unwrap(),
+                api::LandedIn {
+                    channels: vec![api::Channel("nixos-unstable".to_owned())]
+                }
+            );
+        }
+        .boxed()
+    })
+    .await;
+}
