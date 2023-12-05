385befc49b7a0b3f1977a6b3e811d23f812f35a6
Jeremy Fleischman <jeremyfleischman@gmail.com>
2023-12-04 22:49:54 -0800

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:fetcher/src/github.rs

--- fetcher/src/github.rs
+++ fetcher/src/github.rs
@@ -1,9 +1,15 @@
+use graphql_client::GraphQLQuery;
+
 const API_URL: &str = "https://api.github.com/graphql";
 
 pub struct GitHub {
-    client: reqwest::Client
+    client: reqwest::Client,
 }
 
+type DateTime = chrono::DateTime<chrono::Utc>;
+type URI = String;
+type HTML = String;
+type GitObjectID = String;
 
 #[derive(Debug, GraphQLQuery)]
 #[graphql(
@@ -13,5 +19,16 @@ pub struct GitHub {
     variables_derives = "Clone,Debug"
 )]
 pub struct PullsQuery {
+    since: Option<DateTime>,
+}
+
+#[derive(Debug, GraphQLQuery)]
+#[graphql(
+    schema_path = "../vendor/github.com/schema.docs.graphql",
+    query_path = "src/queries/branch_contains.graphql",
+    response_derives = "Debug",
+    variables_derives = "Clone,Debug"
+)]
+pub struct BranchContainsQuery {
     since: Option<DateTime>,
 }
