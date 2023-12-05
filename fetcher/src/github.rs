b3671ff6a4c24aabc64603267634267c7c1487bb
Roland Fredenhagen <dev@modprog.de>
2023-12-05 07:43:34 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:fetcher/src/github.rs

--- fetcher/src/github.rs
+++ fetcher/src/github.rs
@@ -3,3 +3,15 @@ const API_URL: &str = "https://api.github.com/graphql";
 pub struct GitHub {
     client: reqwest::Client
 }
+
+
+#[derive(Debug, GraphQLQuery)]
+#[graphql(
+    schema_path = "../vendor/github.com/schema.docs.graphql",
+    query_path = "src/queries/pulls.graphql",
+    response_derives = "Debug",
+    variables_derives = "Clone,Debug"
+)]
+pub struct PullsQuery {
+    since: Option<DateTime>,
+}
