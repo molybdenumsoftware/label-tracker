5c5b1ef8c43cadd3fe06ab39048be3f2f1e9b149
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-12-08 13:48:41 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:fetcher/src/repo.rs

--- fetcher/src/repo.rs
+++ fetcher/src/repo.rs
@@ -6,23 +6,21 @@ pub async fn fetch_or_clone(
     github_repo: &super::GitHubRepo,
 ) -> anyhow::Result<()> {
     if repo_path.exists() {
-        // gix::open(...)
-        // fetch
-        todo!()
-    } else {
-        let mut fetcher = gix::clone::PrepareFetch::new(
-            github_repo.url(),
-            repo_path,
-            gix::create::Kind::Bare,
-            gix::create::Options::default(),
-            gix::open::Options::default(),
-        )?;
-
-        let should_interrupt = AtomicBool::new(false);
-        let fetch_task = fetcher.fetch_only(gix::progress::Discard, &should_interrupt);
-        //<<< drop(fetcher);
-        let fetch_result = fetch_task.await;
-        fetch_result?;
         Ok(())
+    } else {
+        // let mut fetcher = gix::clone::PrepareFetch::new(
+        //     github_repo.url(),
+        //     repo_path,
+        //     gix::create::Kind::Bare,
+        //     gix::create::Options::default(),
+        //     gix::open::Options::default(),
+        // )?;
+
+        // let should_interrupt = AtomicBool::new(false);
+        // let fetch_task = fetcher.fetch_only(gix::progress::Discard, &should_interrupt);
+        // //<<< drop(fetcher);
+        // let fetch_result = fetch_task.await;
+        // fetch_result?;
+        // Ok(())
     }
 }
